1e3c74d2cc38a465b5c1bd84bc95754acd1ab71f
Star7 <1441609372@qq.com>
2025-07-08 19:31:19 +0800

    kernel: implement susfs v1.5.8

--- kernel_patches/include/linux/susfs.h
+++ kernel_patches/include/linux/susfs.h
@@ -8,7 +8,7 @@
 #include <linux/path.h>
 #include <linux/susfs_def.h>
 
-#define SUSFS_VERSION "v1.5.8"
+#define SUSFS_VERSION "v1.5.9"
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
 #define SUSFS_VARIANT "NON-GKI"
 #else
@@ -40,15 +40,13 @@ struct st_susfs_sus_path_list {
 };
 
 struct st_android_data_path {
-	unsigned long                    i_ino;
-	unsigned long                    s_magic;
 	char                             pathname[SUSFS_MAX_LEN_PATHNAME];
+	bool                             is_inited;
 };
 
 struct st_sdcard_path {
-	unsigned long                    i_ino;
-	unsigned long                    s_magic;
 	char                             pathname[SUSFS_MAX_LEN_PATHNAME];
+	bool                             is_inited;
 };
 #endif
 
@@ -194,6 +192,9 @@ struct filename* susfs_get_redirected_path(unsigned long ino);
 int susfs_get_sus_su_working_mode(void);
 int susfs_sus_su(struct st_sus_su* __user user_info);
 #endif
+
+int susfs_get_enabled_features(char __user* buf, size_t bufsize);
+
 /* susfs_init */
 void susfs_init(void);
 
